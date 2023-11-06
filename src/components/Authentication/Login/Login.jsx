4b84e23f2c265ea54dd26f338b3fb92b00988c46
unknown <sakibbangla49@gmail.com>
2023-11-06 13:17:53 -0800

    signin signup page validation

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -1,18 +1,34 @@
 
 import { Link,  useLocation, useNavigate } from "react-router-dom";
 import image from "../../../assets/Banner-img/loginimage.jpg"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 const Login = () => {
     const {signInUser}=useContext(AuthContext)
     const location=useLocation()
     const navigate=useNavigate()
+    const [error,setError]=useState()
+    const [emailError,setEmailError]=useState()
+    const [passwordError,setPasswordError]=useState()
+    
     const handlelogin=(e)=>{
         e.preventDefault()
         const form =new FormData(e.currentTarget)
         const email=form.get('email')
         const password=form.get('password')
         console.log(email,password)
+
+        setError('');
+        setEmailError('');
+        setPasswordError('')
+
+        //errors handle
+        if(email.valueOf === ''){
+            setEmailError('Email field is required')
+        }
+        if(password.valueOf === ''){
+            setPasswordError('Password field is required')
+        }
         signInUser(email,password)
         .then(result=>{
             console.log(result.user)
@@ -20,6 +36,7 @@ const Login = () => {
         })
         .catch(error=>{
             console.error(error )
+            setError('Try agin email or password is wrong')
         })
 
     }
@@ -36,17 +53,26 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl shadow">
                 <h3 className="font-semibold text-3xl text-[#FFD700]">Login</h3>
                     <form onSubmit={handlelogin} className="card-body">
+                    {
+                           error && <p className="text-lg text-red-900 font-medium">{error}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                            <input type="email" placeholder="email" name="email" className="input input-bordered"  />
                         </div>
+                        {
+                           emailError &&  <p className="text-lg text-red-900 font-medium">{emailError}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                            <input type="password" placeholder="password" name="password" className="input input-bordered"  />
+                            {
+                                passwordError && <p className="text-lg text-red-900 font-medium">{passwordError}</p> 
+                            }
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
--- src/components/Authentication/SignUp/SignUp.jsx
+++ src/components/Authentication/SignUp/SignUp.jsx
@@ -1,10 +1,16 @@
 
 import { Link } from "react-router-dom";
 import image from "../../../assets/Banner-img/loginimage.jpg"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 const SignUp = () => {
     const {createUser}=useContext(AuthContext);
+    const [error,setError]=useState()
+    const [nameError,setNameError]=useState()
+    const [emailError,setEmailError]=useState()
+    const [photoError,setPhotoError]=useState()
+    const [passwordError,setPasswordError]=useState()
+
     const handleSignup=(e)=>{
         e.preventDefault();
         const form= new FormData(e.currentTarget)
@@ -13,12 +19,45 @@ const SignUp = () => {
         const photoURL=form.get('photo');
         const password=form.get('password');
         console.log(displayName,email,photoURL,password)
+
+        setError('');
+        setNameError('');
+        setEmailError('');
+        setPhotoError('');
+        setPasswordError('');
+         //errors handle
+         if(name.valueOf === ''){
+            setNameError('Name field is required')
+        }
+         if(email.valueOf === ''){
+            setEmailError('Email field is required')
+        }
+         if(photo.valueOf === ''){
+            setPhotoError('Photo Url field is required')
+        }
+
+        if(password.valueOf === ''){
+            setPasswordError('Password field is required')
+            return
+        } else if (password.length < 6) {
+            setPasswordError('Password must be at least 6 characters!')
+            return;
+      
+          }
+          else if (!/^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).+$/.test(password)) {
+            setPasswordError(
+              'Password must contain  number, uppercase letter & a special chracter!')
+            return;
+      
+          }
+
         createUser(email,password)
         .then(result=>{
             console.log(result.user)
         })
         .catch(error=>{
             console.error(error);
+            setError('Email already taken or something is wrong')
         })
 
     }
@@ -33,30 +72,45 @@ const SignUp = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl shadow-[#FFD700] bg-[#e5e9ec]">
                 <h3 className="font-semibold text-3xl text-[#FFD700]">SignUp</h3>
                     <form onSubmit={handleSignup} className="card-body">
+                        {
+                            error && <p className="text-lg text-red-900 font-medium">{error}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
                             <input type="text" placeholder="Enter name.." name="name" className="input input-bordered"  />
                         </div>
+                        {
+                            nameError && <p className="text-lg text-red-900 font-medium">{nameError}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="text" placeholder="Enter email.." name="email" className="input input-bordered"  />
                         </div>
+                        {
+                            emailError && <p className="text-lg text-red-900 font-medium">{emailError}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Photo URL</span>
                             </label>
                             <input type="text" placeholder="Enter photo url.." name="photo" className="input input-bordered"  />
                         </div>
+                        {
+                            photoError && <p className="text-lg text-red-900 font-medium">{photoError}</p> 
+                        }
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" placeholder="password" name="password" className="input input-bordered"  />
                         </div>
+                        {
+                            passwordError && <p className="text-lg text-red-900 font-medium">{passwordError}</p> 
+                        }
                         <div className="form-control mt-6">
                             <button type="submit" className="py-3 px-2 rounded font-semibold text-2xl bg-[#FFD700] text-[#fff]">SignUp</button>
                         </div>
