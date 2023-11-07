0b835c8bcffac4b0526ec2413dcb70611921965f
unknown <sakibbangla49@gmail.com>
2023-11-07 02:13:33 -0800

    add food backend

--- src/components/AddFood/AddFood.jsx
+++ src/components/AddFood/AddFood.jsx
@@ -3,12 +3,43 @@ import FoodTableItem from "./FoodTableItem";
 
 
 const AddFood = () => {
+
+    const handleAddFood=(e)=>{
+        e.preventDefault();
+
+        const form=e.target;
+        const food_name=form.food_name.value;
+        const image=form.image.value;
+        const category=form.category.value;
+        const quantity=form.quantity.value;
+        const price=form.price.value;
+        const addby=form.addby.value;
+        const food_origin=form.food_origin.value;
+        const description=form.description.value;
+
+        const addFood={food_name,image ,category,quantity,
+             price,addby,food_origin,description}
+             console.log(addFood)
+
+             fetch('http://localhost:5000/food',{
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(addFood)
+
+             })
+             .then(res=>res.json())
+             .then(data => {
+                console.log(data);
+             })
+    }
     return (
         <div>
             <div className="hero min-h-screen my-10">
                 <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-[#FFD700]">
                     <h3 className="text-xl text-[#00072D] font-semibold">Add Food Form</h3>
-                    <form className="card-body">
+                    <form onSubmit={handleAddFood} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Food Name</span>
@@ -55,10 +86,10 @@ const AddFood = () => {
                             <label className="label">
                                 <span className="label-text">Description </span>
                             </label>
-                            <input type="text" placeholder="Enter Food description " name="description " className="input input-bordered" />
+                            <input type="text" placeholder="Enter Food descriptio" name="description" className="input input-bordered" />
                         </div>
                         <div className="form-control mt-6">
-                            <button className=" btn btn-outline text-[#00072D] py-2 px-4 font-semibold rounded">Add Item</button>
+                            <button type="submit" className=" btn btn-outline text-[#00072D] py-2 px-4 font-semibold rounded">Add Item</button>
                         </div>
                     </form>
                 </div>
