de6d66617def480cb7b58d6c3b2cb3e5c23aabd4
unknown <sakibbangla49@gmail.com>
2023-11-07 12:38:36 -0800

    add loder image

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import { Children, useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import loadingimg from "../../assets/Banner-img/loading.png"
 
 
 const PrivateRoute = () => {
     const {user,loading}=useContext(AuthContext);
     const location=useLocation()
     if(loading){
-        return <p>Loading....</p>
+        return <p className="flex justify-center items-center"><img src={loadingimg} alt="" /></p>
     }
 
     if(user){
